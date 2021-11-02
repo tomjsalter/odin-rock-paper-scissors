ecd48b286906faad59b0ba44be30c2f0406dd65b
Tom Salter <tomjsalter@protonmail.com>
2021-11-02 17:12:40 +0000

    playRound function working

--- script.js
+++ script.js
@@ -10,6 +10,47 @@ function computerPlay() {
   } else if (generateChoice === 2) {
     generateChoice = "Scissors";
   }
-  console.log(generateChoice);
+  console.log(`Computer input: ${generateChoice}.`);
+  return generateChoice;
 }
 
+// create playerPlay function
+function playerPlay() {
+  let playerChoice;
+  let lowerCaseString;
+  let upperCaseChar;
+
+  playerChoice = prompt("Choose Rock, Paper or Scissors?");
+  console.log(playerChoice);
+  upperCaseChar = playerChoice.charAt(0).toUpperCase();
+  lowerCaseString = playerChoice.substring(1).toLowerCase();
+  playerChoice = upperCaseChar + lowerCaseString;
+  console.log(playerChoice);
+  return playerChoice;
+}
+
+// create playRound function
+function playRound(playerSelection, computerSelection) {
+  console.log(`Player chose: ${playerSelection}.`);
+  console.log(`Computer chose: ${computerSelection}.`);
+
+    if (playerSelection === computerSelection) {
+    return "It's a tie";
+  } else if (
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper")
+  ) {
+    return "Player wins!";
+  } else if (
+    (computerSelection === "Rock" && playerSelection === "Scissors") ||
+    (computerSelection === "Paper" && playerSelection === "Rock") ||
+    (computerSelection === "Scissors" && playerSelection === "Paper")
+  ) {
+    return "Computer wins!";
+  }
+}
+
+let playerSelection = playerPlay();
+let computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
